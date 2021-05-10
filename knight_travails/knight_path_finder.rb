612da3ff75d6178c776b00e52d8b913884c11b64
adavies <arielpdavies@gmail.com>
2021-05-10 11:18:26 -0400

    Created valid moves, and new move positions

--- knight_travails/knight_path_finder.rb
+++ knight_travails/knight_path_finder.rb
@@ -7,50 +7,41 @@ class KnightPathFinder
     def self.valid_moves(pos) # [0,1]
         valid = []
         row, col = pos 
-        nums = [1, 3]
-        # while valid.length <= 8
-        #   nums.each do |num|
+        a = [-3,3]
+        b = [1,-1]
 
-        #   end
-          
-        # end
-        4.times do
-          valid << [row + nums[0], col + nums[1]]
-          valid << [row - nums[0], col + nums[1]]
-        end
-          nums.each do |num|
-            valid << [row+num, col]
+        (0...2).each do |i|
+          (0...2).each do |j|
+            valid << [(row + a[i]), (col + b[j])]
+              valid << [(row + b[j]), (col + a[i])]
           end
+        end
 
-        valid
+        valid.reject do |row,col| 
+          row < 0 || row > 7  ||  col < 0 || col > 7
+        end
     end
+
   def initialize(position)
     @root_node = PolyTreeNode.new(position)
+    @considered_positions = [position]
   end
+
+  def new_move_positions(position)
+    poss_positions = KnightPathFinder.valid_moves(position)
+    current_positions = []
+    poss_positions.each do |pos| 
+      if !@considered_positions.include?(pos)
+        @considered_positions << pos
+        current_positions << pos
+      end
+    end
+
+    current_positions
+  end
+
+
 end
-=begin
 
 
-8x8, based off a given position, get all possible moves
-Case for north:
-  Left: position[r-3][c-1]
-  Right: position[r-3][c+1]
-Case for south:
-  Left: position[r+3][c-1]
-  Right: position[r+3][c+1]
-Case for east:
-  Up: position[r-1][c+3]
-  Down: position[r+1][c+3]
-Case for west:
-  Up: position[r-1][c-3]
-  Down: position[r+1][c-3]
-  0 1 2 3 4 5 6 7
-0   X               => [[1, 3], [3, 1]]
-1         O
-2 
-3   O
-4
-5
-6
-7
-=end
+print KnightPathFinder.valid_moves([0,0])
